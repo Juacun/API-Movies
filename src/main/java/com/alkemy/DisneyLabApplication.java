c9fd0ad060f152bced07d526a1db95732d70f377
Juacun <juanpablo.cundere@gmail.com>
2022-08-04 00:03:02 -0300

    Initial Commit

---
+++ src/main/java/com/alkemy/DisneyLabApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/app/DisneyLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
