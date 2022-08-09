d3ffe1466fd6f4724d36310f374d6462e23b38d2
Juacun <juanpablo.cundere@gmail.com>
2022-08-09 13:19:47 -0300

    Creating implementations of entity services

---
+++ src/main/java/com/alkemy/service/GenreServiceImpl.java
@@ -0,0 +1,66 @@
+package com.alkemy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alkemy.entity.GenreEntity;
+import com.alkemy.interfaces.GenreService;
+import com.alkemy.modelDTO.GenreDTO;
+import com.alkemy.modelMapper.GenreMapper;
+import com.alkemy.repository.GenreRepository;
+
+@Service
+public class GenreServiceImpl implements GenreService{
+	
+	GenreRepository genreRepository;
+	GenreMapper genreMapper;
+	
+	@Autowired
+	public GenreServiceImpl(GenreRepository genreRepository, GenreMapper genreMapper) {
+		
+		this.genreRepository = genreRepository;
+		this.genreMapper = genreMapper;
+	}
+
+	
+	@Override
+	public GenreDTO getGenreById(Long id) {
+		
+		GenreEntity genreEntity = genreRepository.getReferenceById(id);
+		GenreDTO result = genreMapper.genreEntity2DTO(genreEntity);
+		return result;
+	}
+
+	@Override
+	public List<GenreDTO> getAllGenre() {
+		
+		List<GenreEntity> genreEntityList = genreRepository.findAll();
+		List<GenreDTO> result = genreMapper.genreEntityList2DTOList(genreEntityList);
+		return result;
+	}
+
+	@Override
+	public GenreDTO saveGenre(GenreDTO genreDTO) {
+		
+		GenreEntity genreEntity = genreMapper.genreDTO2Entity(genreDTO);
+		GenreEntity genreEntitySaved = genreRepository.save(genreEntity);
+		GenreDTO result = genreMapper.genreEntity2DTO(genreEntitySaved);
+		return result;
+	}
+
+	@Override
+	public GenreDTO updateGenre(Long id, GenreDTO genreDTO) {
+		
+		genreDTO.setId(id);
+		return saveGenre(genreDTO);
+	}
+
+	@Override
+	public void deleteGenreById(Long id) {
+		
+		genreRepository.deleteById(id);
+	}
+	
+}
---
+++ src/main/java/com/alkemy/service/MovieServiceImpl.java
@@ -0,0 +1,66 @@
+package com.alkemy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alkemy.entity.MovieEntity;
+import com.alkemy.interfaces.MovieService;
+import com.alkemy.modelDTO.MovieDTO;
+import com.alkemy.modelMapper.MovieMapper;
+import com.alkemy.repository.MovieRepository;
+
+@Service
+public class MovieServiceImpl implements MovieService{
+
+	MovieRepository movieRepository;
+	MovieMapper movieMapper;
+	
+	@Autowired
+	public MovieServiceImpl(MovieRepository movieRepository, MovieMapper movieMapper) {
+		
+	this.movieRepository = movieRepository;
+	this.movieMapper = movieMapper;
+	}
+	
+	
+	@Override
+	public MovieDTO getMovieById(Long id) {
+
+		MovieEntity movieEntity  = movieRepository.getReferenceById(id);
+		MovieDTO result = movieMapper.movieEntity2DTO(movieEntity);
+		return result;
+	}
+
+	@Override
+	public List<MovieDTO> getAllMovie() {
+
+		List<MovieEntity> movieEntityList = movieRepository.findAll();
+		List<MovieDTO> result = movieMapper.movieEntityList2DTOList(movieEntityList);
+		return result;
+	}
+
+	@Override
+	public MovieDTO saveMovie(MovieDTO movieDTO) {
+		
+		MovieEntity movieEntity = movieMapper.movieDTO2Entity(movieDTO);
+		MovieEntity movieEntitySaved = movieRepository.save(movieEntity);
+		MovieDTO result = movieMapper.movieEntity2DTO(movieEntitySaved);
+		return result;
+	}
+
+	@Override
+	public MovieDTO updateMovie(Long id, MovieDTO movieDTO) {
+		
+		movieDTO.setId(id);
+		return saveMovie(movieDTO);
+	}
+
+	@Override
+	public void deleteMovieById(Long id) {
+		
+		movieRepository.deleteById(id);
+	}
+	
+}
